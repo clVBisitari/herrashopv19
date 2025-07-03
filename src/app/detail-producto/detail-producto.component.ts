7e5a524c2f04f854f57768c389fffcc1e0112cdc
tomycernik <cerniktomy@gmail.com>
2025-07-03 17:34:25 -0300

    feat: mejorar UI del componente detalle producto
    - Quitar ID del título para mejor UX
    - Agregar altura fija a imagen para consistencia visual
    - Mejorar responsive design con object-cover
    - Optimizar layout del componente

---
+++ src/app/detail-producto/detail-producto.component.ts
@@ -0,0 +1,61 @@
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { ProductoService } from '../services/producto.service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Producto } from '../interfaces/producto.interface';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { BadgeModule } from 'primeng/badge';
+import { TagModule } from 'primeng/tag';
+
+@Component({
+  selector: 'app-detail-producto',
+  imports: [BrowserModule,
+    FormsModule,
+    CardModule,
+    ButtonModule,
+    BadgeModule,
+    TagModule,
+    RouterLink
+  ],
+  templateUrl: './detail-producto.component.html',
+  styleUrl: './detail-producto.component.css'
+})
+export class DetailProductoComponent implements OnInit, OnDestroy{
+  productoService = inject(ProductoService); 
+
+  activatedRouter = inject(ActivatedRoute);
+
+  id: number | null = null;
+
+  producto: Producto | undefined;
+
+  ngOnInit() {
+    this.id = Number(this.activatedRouter.snapshot.paramMap.get('id'))
+    this.getProducto();  
+  }
+
+  ngOnDestroy(): void {
+    
+  }
+
+  getProducto() {
+    if (this.id !== null) {
+      this.productoService.getProducto(this.id).subscribe(
+        {
+          next : (data)=>{
+            this.producto = data;
+            console.log(data);
+          }, 
+
+          error: (err) => {},
+
+          complete: () => {}
+        }
+      );
+    } else {
+      console.error('El id del producto es null');
+    }
+  }
+}
