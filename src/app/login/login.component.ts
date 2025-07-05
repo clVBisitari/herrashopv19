51410ff7a543dd315f1aeed6132d0618152f47eb
Mariano Saldivar <odioescribirel@hotmail.com>
2025-07-05 12:28:12 -0300

    cambio carpeta imagenes a lugar mas interno, cambios en login y registro

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,19 +3,27 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PasswordModule } from 'primeng/password';  // Importing PasswordModule from PrimeNG
-import { HttpClient } from '@angular/common/http';  
+import { HttpClient } from '@angular/common/http';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterLink ,ReactiveFormsModule, PasswordModule, InputTextModule, ButtonModule],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule, PasswordModule, InputTextModule, ButtonModule],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
-goToRegister() {
-throw new Error('Method not implemented.');
+  goToRegister() {
+    this.http.get('/api/register').subscribe({
+      next: res => {
+        console.log('✅ Redirigiendo a registro', res);
+      },
+      error: err => {
+        console.error('❌ Error al redirigir a registro:', err);
+        alert('Error del servidor al intentar redirigir a registro.');
+      }
+    });
 }
   formGroup: FormGroup;
   value3: string = ''; // Example variable for two-way binding  
@@ -28,15 +36,15 @@ throw new Error('Method not implemented.');
   }
 
   onSubmit() {
-  this.http.post('/api/login', this.formGroup.value).subscribe({
-    next: res => {
-      console.log('✅ Login exitoso', res);
-    },
-    error: err => {
-      console.error('❌ Error al loguear:', err);
-      alert('Error del servidor al intentar iniciar sesión.');
-    }
-  });
-}
+    this.http.post('/api/login', this.formGroup.value).subscribe({
+      next: res => {
+        console.log('✅ Login exitoso', res);
+      },
+      error: err => {
+        console.error('❌ Error al loguear:', err);
+        alert('Error del servidor al intentar iniciar sesión.');
+      }
+    });
+  }
 
 }
