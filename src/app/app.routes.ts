d4e3584ae52b572cbb7afa80fb0a7c375601f85e
Mariano Saldivar <odioescribirel@hotmail.com>
2025-07-07 00:27:44 -0300

    cambios en footer y rutas front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,29 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
   {
+    path: '',
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+  },
+  {
     path: 'login',
     component: LoginComponent
+  },
+  { 
+    path: 'register', 
+    loadComponent: () => import('./register/register.component').then(m => m.RegisterComponent)
+    // loadChildren: () => import('./register/register.routes').then(m => m.registerRoutes),
+    // component: RegisterComponent 
   },
   {
     path: 'productos',
     loadChildren: () => import('./modules/productos/pages/productos.routes').then(e => e.productosRoutes)
   },
-  {
-    path: '',
-    redirectTo: '/login',
-    pathMatch: 'full'
-  }
+  // {
+  //   path: '',
+  //   redirectTo: '/login',
+  //   pathMatch: 'full'
+  // }
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,12 @@
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Route, RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PasswordModule } from 'primeng/password';  // Importing PasswordModule from PrimeNG
 import { HttpClient } from '@angular/common/http';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router'; // Importing Router for navigation
 
 @Component({
   selector: 'app-login',
@@ -14,26 +15,32 @@ import { ButtonModule } from 'primeng/button';
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
-  goToRegister() {
-    this.http.get('/api/register').subscribe({
-      next: res => {
-        console.log('✅ Redirigiendo a registro', res);
-      },
-      error: err => {
-        console.error('❌ Error al redirigir a registro:', err);
-        alert('Error del servidor al intentar redirigir a registro.');
-      }
-    });
-}
-  formGroup: FormGroup;
-  value3: string = ''; // Example variable for two-way binding  
-  constructor(private fb: FormBuilder, private http: HttpClient) {
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
     console.log('FormBuilder:', this.fb);
     this.formGroup = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
     });
   }
+
+  // goToRegister() {
+  //   this.http.get('/api/register').subscribe({
+  //     next: res => {
+  //       console.log('✅ Redirigiendo a registro', res);
+  //     },
+  //     error: err => {
+  //       console.error('❌ Error al redirigir a registro:', err);
+  //       alert('Error del servidor al intentar redirigir a registro.');
+  //     }
+  //   });
+  // }
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
+
+  formGroup: FormGroup;
+  value3: string = ''; // Example variable for two-way binding  
+  
 
   onSubmit() {
     this.http.post('/api/login', this.formGroup.value).subscribe({
